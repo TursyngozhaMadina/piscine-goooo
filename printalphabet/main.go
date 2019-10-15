7bc084c92d04938b1c20953181bbeb16be678c44
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-16 04:11:47 +0600

    alphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,12 +1,9 @@
 package main
 import "github.com/01-edu/z01"
-func main()
-{
-	for i := 'a'; i <='z'; i++ 
-	{
-        z01.PrintRune(i)
-    }
-  z01.PrintRune('\n')
+func main() {
+	for i:='a'; i<='z'; i++ {
+		z01.PrintRume(i)
+
+	}
+	z01.PrintRume('\n')
 }
-
-
